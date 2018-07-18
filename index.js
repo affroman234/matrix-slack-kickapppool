6aa19f8388acfe878f73f116338a11255da4a09e
Jack Streed <jstreed@matrixgroup.net>
2018-07-18 11:25:11 -0400

    'WIP'

--- index.js
+++ index.js
@@ -3,5 +3,57 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
+const AWS = require('aws-sdk');
+const qs = require('querystring');
+const SSH = require('simple-ssh');
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+const kmsEncryptedToken = process.env.kmsEncryptedToken;
+let token;
+let dbResponse = [];
+var dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+
+function processEvent(event, callback) {
+    
+    const params = qs.parse(event.body);
+    const requestToken = params.token;
+    if (requestToken !== token) {
+        console.error(`Request token (${requestToken}) does not match expected`);
+        return callback('Invalid request token');
+    }
+
+    let user = params.user_name;
+    let command = params.command;
+    let channel = params.channel_name;
+    let slackText = params.text;
+
+    callback(null, `Hello ${user} in ${channel}.`);
+}
+
+exports.handler = (event, context, callback) => {
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? (err.message || err) : JSON.stringify(res, null, "\t"),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (token) {
+        // Container reuse, simply process the event with the key in memory
+        processEvent(event, done);
+    } else if (kmsEncryptedToken && kmsEncryptedToken !== '<kmsEncryptedToken>') {
+        const cipherText = { CiphertextBlob: new Buffer(kmsEncryptedToken, 'base64') };
+        const kms = new AWS.KMS();
+        kms.decrypt(cipherText, (err, data) => {
+            if (err) {
+                console.log('Decrypt error:', err);
+                return done(err);
+            }
+            token = data.Plaintext.toString('ascii');
+            processEvent(event, done);
+        });
+    } else {
+        done('Token has not been set.');
+    }
+};
